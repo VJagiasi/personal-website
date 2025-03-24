44e9a13439754184fc74c6883f0b3d8009afdd10
Vihaan Jagiasi <vihaan@jagiasi.com>
2025-03-23 20:59:25 -0400

    update principles

--- app/principles/page.tsx
+++ app/principles/page.tsx
@@ -61,6 +61,22 @@ export default function PrinciplesPage() {
             in initial stages when starting off or learning something new as a complete beginner you shouldn’t worry too much about where to begin or what resources to use - you should care more about how much should you be doing and if you’re even doing enough?
           </p>
         </motion.div>
+
+        <motion.div className="space-y-2 border-b border-dotted border-neutral-300 pb-4" variants={item}>
+          <p className="text-base leading-relaxed text-neutral-800 pl-4 relative">
+          <span className="font-semibold decoration-dotted decoration-neutral-400 underline underline-offset-4 hover:decoration-neutral-800 transition-colors">4. whatever it takes, get it done: </span>
+            <span className="absolute left-0 top-[0.5em] w-1 h-1 bg-neutral-300 rounded-full" />
+            there is no excuse. if you didn’t get the thing done it’s not cause of x,y and z reasons - it’s cause you didn’t want it bad enough and that’s okay but say that with a straight face - don’t lie to me and be like this thing got in the way - no it’s just cause you didn’t want it bad enough that's it.
+          </p>
+        </motion.div>
+
+        <motion.div className="space-y-2 border-b border-dotted border-neutral-300 pb-4" variants={item}>
+          <p className="text-base leading-relaxed text-neutral-800 pl-4 relative">
+          <span className="font-semibold decoration-dotted decoration-neutral-400 underline underline-offset-4 hover:decoration-neutral-800 transition-colors">5. don't tolerate bullshit: </span>
+            <span className="absolute left-0 top-[0.5em] w-1 h-1 bg-neutral-300 rounded-full" />
+            this speaks for itself - no matter what you do - do not ever tolerate bullshit from anyone - i value and respect my time more than anything else and if someone doesn’t - i’m don’t give a shit about them.
+          </p>
+        </motion.div>
       </div>
     </motion.main>
   )
